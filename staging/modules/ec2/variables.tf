50bdaf37251da51b6c64156f49f20a630b66dbc2
visala123 <visala.k@algorims.com>
2025-04-30 18:32:42 +0800

    commit to main

--- staging/modules/ec2/variables.tf
+++ staging/modules/ec2/variables.tf
@@ -22,3 +22,17 @@ variable "key_name" {
   type        = string
   description = "Key pair name for SSH access"
 }
+variable "jumpbox_role_name" {
+  description = "Name of the IAM role for the EC2 jumpbox"
+  type        = string
+}
+
+variable "jumpbox_instance_profile_name" {
+  description = "Name of the IAM instance profile for EC2 jumpbox"
+  type        = string
+}
+
+variable "jumpbox_policy_name" {
+  description = "Name of the IAM policy for EKS access from EC2"
+  type        = string
+}
